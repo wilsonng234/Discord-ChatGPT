ac2617ad74c72250e81faecf4f020209efb048b7
Wilson Ng <wilsonng234@gmail.com>
2023-05-02 15:04:24 +0800

    Init/set up project (#1)
    * Create local Kubernetes deployment README
    
    * Fix local kubernetes setup README
    
    * Ignore openfaas generated folders
    
    * Set up chatgpt handler
    
    * Create requirements.txt
    
    * Create stack.yml
    
    * Create discord bot

---
+++ bot.py
@@ -0,0 +1,28 @@
+import discord
+import requests
+
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
+intents = discord.Intents.default()
+intents.messages = True
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_message(message):
+    if message.author != client.user:
+        api_url = config.get("API_URL") + "/chatgpt"
+        headers = {"Content-Type": "text/plain"}
+
+        response = requests.post(api_url, data=message.content, headers=headers)
+        content = response.content.decode("utf-8")
+
+        if content:
+            await message.channel.send(content)
+        else:
+            await message.channel.send("Something went wrong. Please try again later.")
+
+client.run(config["DISCORD_BOT_TOKEN"])
---
+++ chatgpt/handler.py
@@ -0,0 +1,8 @@
+def handle(req):
+    """handle a request to the function
+    Args:
+        req (str): request body
+    """
+    req = "this is a testing message"
+
+    return req
